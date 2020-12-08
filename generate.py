01ed1ecae7afeef0ad1ee2bb5b1d8f8df55528d1
Jalen <jalenchuh@gmail.com>
2020-12-09 00:18:09 +0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -5,7 +5,7 @@ import requests
 sitemap = 'https://listenlive.cn/sitemap.xml'
 
 html = urllib.request.urlopen(sitemap).read().decode('utf-8')
-result = re.findall(re.compile(r'(?<=<loc>).*?(?=</loc>)'), html)
+result = re.findall(re.compile(r'(?<=<url>).*?(?=</url>)'), html)
 
 with open('urls.txt', 'w') as file:
   for data in result:
