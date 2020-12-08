12f1c9f867fb05df080a16693e1b6d8def56a088
gaoxianghua <listenlive@126.com>
2020-12-08 18:14:51 +0800

    20201208

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@ import re
 import urllib
 import requests
 
-sitemap = 'https://blog.jalenchuh.cn/sitemap.xml'
+sitemap = 'https://listenlive.cn/sitemap.xml'
 
 html = urllib.request.urlopen(sitemap).read().decode('utf-8')
 result = re.findall(re.compile(r'(?<=<loc>).*?(?=</loc>)'), html)
