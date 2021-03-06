db3387a3ed4e9c800104b0cb42cd5643735d2a60
Jalen <jalenchuh@gmail.com>
2021-03-06 13:30:46 +0800

    :sparkles: Add Bing submit #2

--- generate.py
+++ generate.py
@@ -1,8 +1,13 @@
 import re
+import json
 import urllib
 import requests
 
+site = 'https://blog.jalenchuh.cn'
 sitemap = 'https://blog.jalenchuh.cn/sitemap.xml'
+
+listUrl = []
+bingData = {}
 
 html = urllib.request.urlopen(sitemap).read().decode('utf-8')
 result = re.findall(re.compile(r'(?<=<loc>).*?(?=</loc>)'), html)
@@ -10,4 +15,11 @@ result = re.findall(re.compile(r'(?<=<loc>).*?(?=</loc>)'), html)
 with open('urls.txt', 'w') as file:
   for data in result:
     print(data, file=file)
+    listUrl.append(data)
+file.close()
+
+bingData["siteUrl"] = site
+bingData["urlList"] = listUrl
+with open("bing.json", "w") as f:
+  json.dump(bingData,f)
 file.close()
