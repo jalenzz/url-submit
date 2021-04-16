2f4c0dd6c7fd87f2e02d23d40c4f24bf467c0d51
XiaoMouz <54032212+XiaoMouz@users.noreply.github.com>
2021-04-16 18:06:20 +0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,13 +1,1 @@
-import re
-import urllib
-import requests
 
-sitemap = 'https://www.mouz.xyz/sitemap/sitemap_1.xml'
-
-html = urllib.request.urlopen(sitemap).read().decode('utf-8')
-result = re.findall(re.compile(r'(?<=<url>).*?(?=</url>)'), html)
-
-with open('urls.txt', 'w') as file:
-  for data in result:
-    print(data, file=file)
-file.close()
