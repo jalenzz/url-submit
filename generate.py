993c1e06305b7b3ec860ceee61f016c96af4d8da
Jalen <jalenchuh@gmail.com>
2021-07-02 23:33:13 +0800

    :bug: Fix 403

--- generate.py
+++ generate.py
@@ -9,17 +9,17 @@ sitemap = 'https://blog.jalenchuh.cn/sitemap.xml'
 listUrl = []
 bingData = {}
 
-html = urllib.request.urlopen(sitemap).read().decode('utf-8')
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}  
+req = urllib.request.Request(url=sitemap, headers=headers)
+html = urllib.request.urlopen(req).read().decode('utf-8')
 result = re.findall(re.compile(r'(?<=<loc>).*?(?=</loc>)'), html)
 
 with open('urls.txt', 'w') as file:
   for data in result:
     print(data, file=file)
     listUrl.append(data)
-file.close()
 
 bingData["siteUrl"] = site
 bingData["urlList"] = listUrl
 with open("bing.json", "w") as f:
   json.dump(bingData,f)
-file.close()
