53d208dc1e2a8981b2b0fd3621ad0498b4e03be0
Jalen <jalenchuh@gmail.com>
2021-07-02 23:21:02 +0800

    Revert "fix 403 err"
    This reverts commit 9b8a3c8948fecc30022af4dea78242c2bd535ad8.

--- generate.py
+++ generate.py
@@ -1,7 +1,6 @@
 import re
 import json
 import urllib
-import ssl
 import requests
 
 site = 'https://yunzd.life'
@@ -10,17 +9,17 @@ sitemap = 'https://yunzd.life/sitemap.xml'
 listUrl = []
 bingData = {}
 
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}  
-req = urllib.request.Request(url=sitemap, headers=headers)
-html = urllib.request.urlopen(req).read().decode('utf-8')
+html = urllib.request.urlopen(sitemap).read().decode('utf-8')
 result = re.findall(re.compile(r'(?<=<loc>).*?(?=</loc>)'), html)
 
 with open('urls.txt', 'w') as file:
   for data in result:
     print(data, file=file)
     listUrl.append(data)
+file.close()
 
 bingData["siteUrl"] = site
 bingData["urlList"] = listUrl
 with open("bing.json", "w") as f:
   json.dump(bingData,f)
+file.close()
